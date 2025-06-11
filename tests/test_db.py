9be520c7a8f386a24552e7194557316e12fc5ad5
LeonardoSanders <leonardosanders54@gmail.com>
2025-06-10 22:28:35 -0400

    Refatorando estrutura do projeto: Suporte a asyncio, tornando o projeto não bloqueante

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,22 +1,27 @@
 from dataclasses import asdict
 
+import pytest
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from fastapi_zero.models import User
 
 
-def test_creat_user(session, mock_db_time):
+@pytest.mark.asyncio
+async def test_creat_user(session: AsyncSession, mock_db_time):
     with mock_db_time(model=User) as time:
         new_user = User(username='test', email='test@test', password='secret')
 
         session.add(new_user)
-        session.commit()
+        await session.commit()
 
-        user = session.scalar(select(User).where(User.username == 'test'))
+        user = await session.scalar(
+            select(User).where(User.username == 'test')
+        )
 
         # breakpoint()
 
-        assert asdict(user) == {
+        assert asdict(user) == {  # type: ignore
             'id': 1,
             'username': 'test',
             'email': 'test@test',
